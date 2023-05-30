7c7cced9dac53efc5274e06fec66f840443a9b68
Lawrence Wang <lwang801118@gmail.com>
2023-05-30 22:07:52 +0000

    added event listener to form to prevent page from refreshing when submitted, stores the inputs in a new object and base the entry ID on the nextEntryId value, pushes the object into the array of objects, and resets the form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
+const $title = document.querySelector('#inputTitle');
 const $url = document.querySelector('#inputUrl');
+const $notes = document.querySelector('#inputNotes');
 const $img = document.querySelector('img');
+const $form = document.querySelector('form');
 
 $url.addEventListener('input', viewImage);
 
 function viewImage(event) {
   $img.src = event.target.value;
+}
+
+$form.addEventListener('submit', saveEntry);
+
+let entryObjects = [];
+const previousEntryObjects = localStorage.getItem(entryObjects);
+let nextEntryId = 1;
+if (previousEntryObjects != null) {
+  entryObjects = JSON.parse(previousEntryObjects);
+  nextEntryId = entryObjects[0].nextEntryId + 1;
+}
+
+function saveEntry(event) {
+  event.preventDefault();
+  const entryObj = {
+    entryId: nextEntryId,
+    title: $title.value,
+    imageUrl: $img.src,
+    notes: $notes.value
+  };
+  entryObjects.unshift(entryObj);
+  $img.src = 'images/placeholder-image-square.jpg';
+  nextEntryId++;
+  $form.reset();
 }
